3ef35b062522e5646f2adbce1e7120e513de74b0
Nishanth Banjan <nishantbanjan210306@gmail.com>
2024-09-19 01:57:31 +0530

    delta

--- hello.c
+++ hello.c
@@ -3,8 +3,11 @@ int main(){
     printf("hello world ");
     printf("%d",3+4);
     
+    dlbqjbjfkeqn;NTDDI_WIN2Kvdejklv fqejk
+     dnqj vhjq, 
+     v fqkl vfjkqe
     2+3;
-    32+23;
-    43+34;
+    31+23+66;
+    44+34;
     43-543;
 }
